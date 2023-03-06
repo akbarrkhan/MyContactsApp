5bc00f528502f6751dab980bb19ba54df26cd369
Muhammad Akbar Khan <akbarrkhan@outlook.com>
2023-03-06 16:50:06 +0500

    Initial Commit

---
+++ MyContactsApp/MyContactsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyContactsAppApp.swift
+//  MyContactsApp
+//
+//  Created by Mac on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyContactsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
